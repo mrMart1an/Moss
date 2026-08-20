ee1410bf54f0e410d5f59fcba09348c8dc5c80e0
Andrea Greco <113620544+mrMart1an@users.noreply.github.com>
2026-08-20 22:15:47 +0200

    daemon installation fix

--- daemon/build.rs
+++
@@ -1,8 +0,0 @@
-const SERVICE_NAME: Option<&str> = option_env!("SERVICE_NAME");
-
-fn main() {
-    // Check if the env are set
-    if SERVICE_NAME.is_none() {
-        panic!("SERVICE_NAME env not defined!");
-    }
-}
